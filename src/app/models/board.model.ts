1e42284a23287fc150a9f138c670ac8c357da295
Arlin Holguin <arlingholguin@gmail.com>
2024-02-07 09:02:33 -0500

    Type mapColors cards

--- src/app/models/board.model.ts
+++ src/app/models/board.model.ts
@@ -3,7 +3,7 @@ import { User } from "./user.model";
 export interface Board {
     id: number;
     title: string;
-    backgroundColor: string;
+    backgroundColor: 'sky' | 'yellow' | 'green' | 'gray' | 'purple' | 'red' | 'violet';
     members: User[];
     creationAt: string;
     updatedAt: string;
--- src/app/modules/shared/components/card-color/card-color.component.ts
+++ src/app/modules/shared/components/card-color/card-color.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-color',
   templateUrl: './card-color.component.html'
 })
-export class CardColorComponent {
+export class CardColorComponent  {
+
+  @Input() color: 'sky' | 'yellow' | 'green' | 'gray' | 'purple' | 'red' | 'violet' = 'sky';
+  mapColors = {
+    sky:{
+      'bg-sky-700': true,
+      'hover:bg-sky-800': true,
+      'text-white': true
+
+    },
+    yellow:{
+      'bg-yellow-700': true,
+      'hover:bg-yellow-800': true,
+      'text-white': true
+    },
+    green:{
+      'bg-green-700': true,
+      'hover:bg-green-800': true,
+      'text-white': true
+    },
+    gray:{
+      'bg-gray-700': true,
+      'hover:bg-gray-800': true,
+      'text-white': true
+    },
+    purple:{
+      'bg-purple-700': true,
+      'hover:bg-purple-800': true,
+      'text-white': true
+    },
+    red:{
+      'bg-red-700': true,
+      'hover:bg-red-800': true,
+      'text-white': true
+    },
+    violet:{
+      'bg-violet-700': true,
+      'hover:bg-violet-800': true,
+      'text-white': true
+    }
+
+
+  }
+
+  constructor() { }
+
+  
+
+  get colors(){
+    const clases = this.mapColors[this.color];
+    return clases ? clases : {};
+    
+  }
 
 }
