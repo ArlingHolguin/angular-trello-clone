a327e15ddf033d1210b33166024dae797dbb1813
Arlin Holguin <arlingholguin@gmail.com>
2024-02-07 16:19:11 -0500

    Creacion del board

--- src/app/modules/layout/components/board-form/board-form.component.ts
+++ src/app/modules/layout/components/board-form/board-form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { Colors } from '@models/colors.model';
+import { BoardsService } from '@services/boards.service';
 
 @Component({
   selector: 'app-board-form',
@@ -10,17 +13,28 @@ export class BoardFormComponent {
   faCheck = faCheck;
   form = this.formBuilder.nonNullable.group({
     title: ['', [ Validators.required]],
-    backgroundColor:['']
+    backgroundColor: new FormControl<Colors>('sky',{
+      nonNullable: true,
+      validators:[Validators.required]
+    })
   })
   
 constructor(
-  private formBuilder : FormBuilder
+  private formBuilder : FormBuilder,
+  private boardService : BoardsService,
+  private router :  Router
 ){}
 
 doSave(){
   if(this.form.valid){
     const { title, backgroundColor} = this.form.getRawValue();
-    console.log('title', title, 'color', backgroundColor);
+    this.boardService.createBoard(title, backgroundColor).subscribe(board =>{
+      this.router.navigate(
+        ['/app/boards', board.id]
+      )
+    }
+      
+    )
     
 
   }else{
